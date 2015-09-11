7c5fbccc1e29379290458c24bb6547a185f64d6c
Romain Durand Saint Omer <romain.dso@gmail.com>
2015-09-11 14:56:43 +0200

    Use lodash for merging base and prod webpack config

---
+++ webpack.prod.config.js
@@ -0,0 +1,29 @@
+var baseConfig = require('./webpack.base.config.js');
+var webpack = require("webpack");
+var _ = require('lodash');
+
+var prodConfig = {
+    module: {
+        loaders: [{
+            test: /\.scss$/,
+            loader: 'style!css!sass'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+};
+
+// Merge the base config with the prod config
+var prodConfig = _.merge(baseConfig, prodConfig, function(a, b) {
+    if (_.isArray(a)) {
+        return a.concat(b);
+    }
+});
+
+module.exports = prodConfig;
