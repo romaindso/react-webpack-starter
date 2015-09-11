53538b5fb4c928ccb0b059963c4ece540ed7ca14
Romain Durand Saint Omer <romain.dso@gmail.com>
2015-09-11 14:54:33 +0200

    Add base config for webpack

---
+++ webpack.base.config.js
@@ -0,0 +1,34 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+    entry: ['./app/index.js'],
+    output: {
+        path: path.join(__dirname, './build'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            }
+        ],
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel?optional[]=runtime'
+            },
+            {
+                test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
+            }
+        ]
+    }
+};
