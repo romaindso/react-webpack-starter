9df9ebcc64ae8ee8f7131d2d7d9b0ec06e1d5e2a
Romain Durand Saint Omer <romain.dso@gmail.com>
2015-09-30 19:14:14 +0200

    Fix indent

--- karma.conf.test.js
+++ karma.conf.test.js
@@ -2,13 +2,13 @@ var baseConfig = require('./karma.conf.js');
 var _ = require('lodash');
 
 var testConfig = {
-  preprocessors: {
-      'tests.webpack.js': ['sourcemap']
-  },
-  reporters: ['spec'],
-  webpack: {
-      devtool: 'inline-source-map'
-  }
+    preprocessors: {
+        'tests.webpack.js': ['sourcemap']
+    },
+    reporters: ['spec'],
+    webpack: {
+        devtool: 'inline-source-map'
+    }
 }
 
 // Merge the base config with the test config
