8e1000fb2bb6bfc2c6af9e0d42ef01823dec8520
Romain Durand Saint Omer <romain.dso@gmail.com>
2015-11-02 22:34:05 +0100

    Fix imports

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'app/App.jsx';
 import ReactTestUtils from 'react-addons-test-utils';
+import App from 'App.jsx';
 
 describe('App', () => {
-  it('should exist', () => {
-    var app = ReactTestUtils.renderIntoDocument('<App />');
-  });
+  it('should exists', () => {
+      var app = ReactTestUtils.renderIntoDocument(<App />);
+      expect(ReactTestUtils.isCompositeComponent(app)).toBeTruthy();
+    });
 });
